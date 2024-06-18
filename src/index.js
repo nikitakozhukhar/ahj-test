d234155628690b6dbd31e48d605bdc083f014149
NikitaKx9 <nikitakx9@gmail.com>
2024-06-18 04:47:07 +0300

    validateCardNumber added

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './css/style.css';
 
 import './js/app';
 
+import vallidateCardNumber from './js/validateCardNumber'
+
+vallidateCardNumber('2720999062135361')
--- src/js/validateCardNumber.js
+++ src/js/validateCardNumber.js
@@ -1,7 +1,25 @@
-export function vallidateCardNumber() {
+export default function vallidateCardNumber(cardNumber) {
   
+  const digits = cardNumber.split('').map(Number)
   
-  
+  for (let i = digits.length - 2; i >= 0; i -= 2) {
+    
+    digits[i] *= 2;
+
+    if (digits[i] > 9) {
+      digits[i] -= 9
+    }
+  }
+
+  const sum = digits.reduce((acc, digit) => acc + digit, 0);
+  const controleNum = digits[digits.length - 1];
+  const calcControleNum = 10 - ((sum - controleNum) % 10);
+
+  if (controleNum === calcControleNum) {
+    console.log('valid')
+  } else {
+    console.log('invalid')
+  }
 }
 
 
